c49b5728337d77da428607146464821157e53283
IrisAlvarado <42157105+IrisAlvarado@users.noreply.github.com>
2018-08-07 22:13:12 -0600

    Update Node.java

--- src/complements/Node.java
+++ src/complements/Node.java
@@ -1,34 +1,34 @@
 package complements;
 
 //Clase para el manejo de nodos para la implementacion de lista
-public class Node {
+public class Nodo {
 	
 	//variable en la que se va a guardar el valor del nodo
-	private Object value;
+	private Object valor;
 	//variable para enlazar nodos
-	private Node next;
+	private Nodo siguiente;
 	
 	//constructor para inicializar los valores de las variables
-	public Node() {
-		this.value = 0;
-		this.next = null;
+	public Nodo() {
+		this.valor = 0;
+		this.siguiente = null;
 	}
 	
 	//metodos set y get para los atributos
 
-	public Object getValue() {
-		return value;
+	public Object getValor() {
+		return valor;
 	}
 
-	public void setValue(Object value) {
-		this.value = value;
+	public void setValor(Object valor) {
+		this.valor = valor;
 	}
 
-	public Node getNext() {
-		return next;
+	public Node getSiguiente() {
+		return siguiente;
 	}
 
-	public void setNext(Node next) {
-		this.next = next;
+	public void setSiguiente(Nodo siguiente) {
+		this.siguiente = siguiente;
 	}
 }
